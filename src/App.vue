23f4fa4ca25b502e0095f765249424c6853d4b5c
eudes-morais <eudes.morais@gmail.com>
2024-10-01 01:10:50 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<!-- Pode-se dizer que é o componente raíz de toda a aplicação -->
+<!-- Isto é um single file component (template + css)-->
+
+<script lang="ts">
+import Banner from './components/Banner.vue';
+
+export default {
+  components: { Banner: Banner }
+  // ou components: { Banner } (As duas maneiras de escrever o componente estão certas)
+}
+</script>
+
+<template>
+  <Banner />
+</template>
+
+<style scoped>
+
+</style>
