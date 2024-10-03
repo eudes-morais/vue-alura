e1dadccce5ce88d9db13b58a308faf5eca43f6cf
eudes-morais <eudes.morais@gmail.com>
2024-10-02 21:46:02 -0300

    Importando dados

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,21 @@
 
 <script lang="ts">
 import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
 
 export default {
-  components: { Banner: Banner }
-  // ou components: { Banner } (As duas maneiras de escrever o componente estão certas)
+  components: { Banner: Banner, ConteudoPrincipal }
+  // ou components: { Banner } (esta é a outro maneira de escrever o componente, já que a chave/valor são os mesmos)
 }
 </script>
 
 <template>
   <Banner />
+  <ConteudoPrincipal />
 </template>
 
+<!-- O SCOPED abaixo referenciado, serve atribuir um ID para os elementos que estão no script, não afetando outros componentes -->
+ <!-- Caso a tag STYLE não tenha o scoped, todos os demais componentes da aplicação serão afetados pelo style globalmente -->
 <style scoped>
 
 </style>
